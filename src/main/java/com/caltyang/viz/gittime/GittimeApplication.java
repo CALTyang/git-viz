ab23fde5d181fabd3d5676bd6a5257c266d2c093
CALTyang <544328917@qq.com>
2020-08-06 22:35:36 +0800

    first commit

---
+++ src/main/java/com/caltyang/viz/gittime/GittimeApplication.java
@@ -0,0 +1,13 @@
+package com.caltyang.viz.gittime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltyang/viz/gittime/GittimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltyang.viz.gittime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
